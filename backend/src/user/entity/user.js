ac458f8d65e323810a6e1358f6e6cab122a559d0
hale-pretty <lnhatha89@gmail.com>
2024-12-20 00:38:29 +0700

    fix import, mapping bug and update readme

--- backend/src/user/entity/user.js
+++ backend/src/user/entity/user.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../../db/sequelize';
+import { sequelize } from '../../db/sequelize';
 
 const User = sequelize.define('User', {
   id: {
@@ -7,14 +7,14 @@ const User = sequelize.define('User', {
     primaryKey: true,
     autoIncrement: true,
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
 }, {
   tableName: 'users',
