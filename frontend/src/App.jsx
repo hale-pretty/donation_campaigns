4753da4d94ca030b0f21601025a82675cf7d0c0d
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-20 23:54:46 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
 // import { Provider } from 'react-redux';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Profile from './pages/Profile/Profile';
 import AppBar from './components/AppBar/AppBar';
 import Router from './pages/router';
 
@@ -7,8 +8,18 @@ const App = () => {
   return (
     // <Provider>
       <BrowserRouter>
-        <AppBar />
-        <Router />
+        <Routes>
+        <Route exact path="/" element={<AppBar />}></Route>
+        <Route exact path="/individuals" element={<Page404 />}></Route>
+        <Route exact path="/individuals/:id">
+          <Route exact path="" element={<Profile path="" />}></Route>
+          <Route exact path="profile" element={<Profile path="" />}></Route>
+          <Route exact path="campaigns" element={<Profile path="campaigns" />}></Route>
+          <Route exact path="contributions" element={<Profile path="contributions" />}></Route>
+          <Route exact path="edit/:edittab" element={<Profile path="edit" />}></Route>
+        </Route>
+        <Route element={<Router />}></Route>
+      </Routes>
       </BrowserRouter>
     // </Provider>
   );
