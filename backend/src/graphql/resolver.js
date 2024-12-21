fcc4ac9e70bd560928c3d0172c6ad132c2d7915b
emerald-lan <emeraldlan1105@gmail.com>
2024-12-22 01:54:19 +0700

    add campaign events

--- backend/src/graphql/resolver.js
+++ backend/src/graphql/resolver.js
@@ -1,7 +1,31 @@
+import { Campaign } from "../campaign/entity/campaign.js";
+import { User } from "../user/entity/user.js";
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello, world!',
-  }
+    campaigns: async () => {
+      return await Campaign.findAll({ include: { model: User, as: 'user' } });
+    },
+    campaign: async (_, { id }) => {
+      return await Campaign.findByPk(id, { include: { model: User, as: 'user' } });
+    },
+  },
+  Mutation: {
+    createCampaign: async (_, args) => {
+      return await Campaign.create(args);
+    },
+    updateCampaign: async (_, { id, ...updateFields }) => {
+      const campaign = await Campaign.findByPk(id);
+      if (!campaign) throw new Error('Campaign not found');
+      return await campaign.update(updateFields);
+    },
+    deleteCampaign: async (_, { id }) => {
+      const campaign = await Campaign.findByPk(id);
+      if (!campaign) throw new Error('Campaign not found');
+      await campaign.destroy();
+      return true;
+    },
+  },
 };
 
 export { resolvers };
