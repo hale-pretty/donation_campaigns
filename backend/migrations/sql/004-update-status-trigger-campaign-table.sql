84302a1c29aa9db8b11aa9e8384cea03c21feac8
emerald-lan <emeraldlan1105@gmail.com>
2024-12-22 13:28:20 +0700

    update migrations

--- backend/migrations/sql/004-update-status-trigger-campaign-table.sql
+++ backend/migrations/sql/004-update-status-trigger-campaign-table.sql
@@ -1,16 +1,16 @@
 CREATE OR REPLACE FUNCTION update_campaign_status()
 RETURNS TRIGGER AS $$
 BEGIN
-  IF CURRENT_DATE >= NEW.end_date THEN
-    NEW.status := 'closed';
-  ELSE
+  IF CURRENT_DATE < NEW.end_date AND NEW.raised_amount < NEW.goal_amount THEN
     NEW.status := 'open';
+  ELSE
+    NEW.status := 'closed';
   END IF;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER campaign_status_trigger
+CREATE OR REPLACE TRIGGER campaign_status_trigger
 BEFORE INSERT OR UPDATE ON "campaigns"
 FOR EACH ROW
 EXECUTE FUNCTION update_campaign_status();
