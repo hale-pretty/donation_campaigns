17dd1be81dc17dcf43d476d1e540dae84dd4634f
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-26 10:28:41 +0700

    Create profile.js

---
+++ frontend/src/routers/profile.js
@@ -0,0 +1,42 @@
+import Page404 from "../pages/Page404";
+import Profile from "../pages/Profile/Profile";
+
+const profileRouters = [
+    {
+        path: "individuals",
+        element: <Page404 />,
+        title: "Error"
+    },
+    {
+        path: "individuals/:idParam",
+        element: <Profile path="" />,
+        title: "Profile"
+    },
+    {
+      path: "individuals/:idParam/profile",
+      element: <Profile path="" />,
+      title: "Profile"
+  },
+    {
+      path: "individuals/:idParam/campaigns",
+      element: <Profile path="campaigns" />,
+      title: "Campaigns"
+    },
+    {
+      path: "individuals/:idParam/contributions",
+      element: <Profile path="contributions" />,
+      title: "Contributions"
+    },
+    {
+      path: "individuals/:idParam/edit",
+      element: <Profile path="edit" />,
+      title: "Edit"
+    },
+    {
+      path: "individuals/:idParam/edit/:edittab",
+      element: <Profile path="edit" />,
+      title: "Edit"
+    },
+];
+
+export default profileRouters;
