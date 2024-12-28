ee9869d1bd4cbd632c4c346e1f5bd1eb95dfba62
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-28 10:30:37 +0700

    Update index.jsx

--- frontend/src/routers/index.jsx
+++ frontend/src/routers/index.jsx
@@ -1,8 +1,12 @@
 import HomePage from "~/pages/Home/HomePage";
 import authRouters from "./auth.jsx";
+import profileRouters from "./profile.jsx";
+import projectRouters from "./project.jsx";
 
 const pagesData = [
   ...authRouters,
+  ...projectRouters,
+  ...profileRouters,
   {
     path: "",
     element: <HomePage />,
