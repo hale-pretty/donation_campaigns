93eea9db71857d5c0631b1da443b3072fdad739d
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-28 14:30:34 +0700

    Update 001-users.sql

--- backend/migrations/sql/001-users.sql
+++ backend/migrations/sql/001-users.sql
@@ -1,5 +1,18 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(512) NOT NULL
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(512) NOT NULL,
+  createdAt DATE NOT NULL,
+  updatedAt DATE NOT NULL,
+  profile_image VARCHAR(255),
+  country VARCHAR(100),
+  city VARCHAR(100),
+  firstname VARCHAR(100),
+  lastname VARCHAR(100),
+  short_description TEXT,
+  about_me TEXT,
+  comments_counter INT DEFAULT 0,
+  campaigns_counter INT DEFAULT 0,
+  contributions_counter INT DEFAULT 0
 );
