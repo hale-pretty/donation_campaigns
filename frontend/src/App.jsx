4e7863d830d3bec6b7953c50b97bf88c49dd0abd
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-28 10:32:18 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import AppBar from './components/AppBar/AppBar';
 import Router from './pages/router';
+import store from "./store";
 
 const App = () => {
   return (
-    // <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <AppBar />
         <Router />
       </BrowserRouter>
-    // </Provider>
+    </Provider>
   );
 };
 
