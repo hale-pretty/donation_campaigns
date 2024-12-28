684c7b65a9815b1ccf9d5bc5301fedf34c29f79c
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-28 15:12:27 +0700

    Update index.js

--- backend/src/user/service/index.js
+++ backend/src/user/service/index.js
@@ -3,132 +3,64 @@ import { User } from '../entity/user.js';
 import bcrypt from "bcrypt";
 import 'dotenv/config';
 import jwt from "jsonwebtoken";
-import { Kind } from 'graphql/language/index.js';
-import { GraphQLScalarType } from 'graphql';
 
+export const getAuthUser = async (user_context) => {
+    if (user_context instanceof Error) {
+        return user_context;
+    } else {
+        return await User.findOne({ where: { id: user_context.user_info.id } })
+            .then((records) => records);
+            .catch((err) => err);
+    }
+};
 
-export const resolvers = {
-    Query: {
-        user(_, __, context_auth) {
-            if (context_auth.user instanceof Error) {
-                return context_auth.user;
-            } else {
-                return db.User.findOne({ where: { id: context_auth.user.user_info.id } })
-                .then((records) => {
-                    if (records.dataValues.profile_image !== null) records.dataValues.profile_image = Buffer.from(records.dataValues.profile_image).toString('binary');
-                    if (records.dataValues.avatar !== null) records.dataValues.avatar = Buffer.from(records.dataValues.avatar).toString('binary');
-                    return records;
-                }).catch((err) => {
-                    
-                    return err;
-                })
-            }
-        },
-        user_by_id(parent, args, context_auth) {
-            return db.User.findOne({ where: { id: args.user_id } })
-            .then((records) => {
-                
-                if (records !== null) {
-                    console.log("records: ", records);
-                    if (records.dataValues.profile_image !== null) records.dataValues.profile_image = Buffer.from(records.dataValues.profile_image).toString('binary');
-                    if (records.dataValues.avatar !== null) records.dataValues.avatar = Buffer.from(records.dataValues.avatar).toString('binary');
-                    return records.dataValues;
-                } else {
-                    throw new Error("No such user id");
-                }
-            }).catch((err) => {
-                
-                return err;
-            })
-        },
-    },
-    UserPublic: {
-        my_campaigns(parent) {
-            return db.Campaign.findAll({ where: { "user_id": parent.id } }).then((records) => {
-                return records;
-            }).catch((err) => {
-                
-                return err;
-            })
-        }
-    },
-    /** 
-    Thanh Lan tao field nay cho Campaign
-    Campaign: {
-        get_images(parent, args, context) {
-            return db.CampaignImage.findAll({ where: { "campaign_id": parent.id }}).then((records) => {
-                return records.map((item) => Buffer.from(item.dataValues.base64_image).toString('binary'));
-            }).catch((err) => {
-                
-                return err;
-            })
-        },
-    },
-    **/
-    User: {
-        my_campaigns(parent) {
-            return db.Campaign.findAll({ where: { "user_id": parent.id } }).then((records) => {
-                return records;
-            }).catch((err) => {
-                
-                return err;
-            })
-        }
-    },
-    Mutation: {
-        async register(parent, args) {
-            const hashedPw =  await bcrypt.hash(args.password, 10);
-            return db.User.create({ username: args.username, password: hashedPw, email: args.email })
-            .then(data => ({ data: data, error: null }))
-            .catch(err => {
-                if (err.name === "SequelizeUniqueConstraintError") return { data: null, error: "username existed" };
-                return err;
-            });
-        },
-        async login(parent, args, context) {
-            return await db.User.findOne({ where: { "username": args.username }})
-            .then(async (records) => {
-                if (records === null) {
-                    return { error: "username not existed", token: null };
-                } else {
-                    const isValidate = await bcrypt.compare(args.password, records.password);
-                    if (isValidate) {
-                        delete records.dataValues.password;
-                        const accessToken = jwt.sign({ id: records.dataValues.id, username: records.dataValues.username }, process.env.SECRET_KEY, { expiresIn: 604_800_000});
-                        return { error: null, token: accessToken };
-                    } else {
-                        return { error: "your password is incorrect", token: null };
-                    }
-                }
-            })
-            .catch((err) => err);            
-        },
-      /**
-      in progess
-        async update_profile(parent, args) {
-            const fields = args.input;
-            const editedUser = await db.User.findOne({ where: { id: fields.user_id } });
-            delete fields.user_id;          
-            await editedUser.update(fields);
-            await editedUser.save();
-            return { message: "sucess", error: null };
-        }
-        **/
-    },
-    Date: new GraphQLScalarType({
-        name: 'Date',
-        description: 'Date custom scalar type',
-        parseValue(value) {
-            return new Date(value); // value from the client
-        },
-        serialize(value) {
-            return value.getTime(); // value sent to the client
-        },
-        parseLiteral(ast) {
-            if (ast.kind === Kind.INT) {
-            return parseInt(ast.value, 10); // ast value is always in string format
-            }
-            return null;
-        },
+export const getPublicUser = async (user_id) => {
+    return await User.findOne({ where: { id: user_id } })
+    .then((records) => {
+        if (records !== null) { return records.dataValues; }
+        else { throw new Error("No such user id"); }
     })
-}
+    .catch((err) => err);
+};
+
+export const register = async (user_info) {
+    const transaction = await sequelize.transaction();
+    const hashedPw = await bcrypt.hash(args.password, 10);
+    try {
+        const newUser = await User.create(
+            { username: user_info.username, password: hashedPw, email: user_info.email },
+            { transaction }
+        )
+        .then(data => ({ data: data, error: null }))
+        .catch(err => {
+            if (err.name === "SequelizeUniqueConstraintError") return { data: null, error: "username existed" };
+            return err;
+        });
+        
+        await transaction.commit();
+        return newUser;
+    } catch (error) {
+        await transaction.rollback();
+        console.error("error creating donation:", error);
+        throw error;
+    }
+};
+
+export const login = async login(username, password) {
+    return await User.findOne({ where: { "username": username }})
+    .then(async (records) => {
+        if (records === null) {
+            return { error: "username not existed", token: null };
+        } else {
+            const isValidate = await bcrypt.compare(password, records.password);
+            if (isValidate) {
+                delete records.dataValues.password;
+                const accessToken = jwt.sign({ id: records.dataValues.id, username: records.dataValues.username }, process.env.SECRET_KEY, { expiresIn: 604_800_000});
+                return { error: null, token: accessToken };
+            } else {
+                return { error: "your password is incorrect", token: null };
+            }
+        }
+    })
+    .catch((err) => err);
+};
