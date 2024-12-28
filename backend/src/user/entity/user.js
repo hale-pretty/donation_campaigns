1de230d872b31ce029d2f42a9f9f904833771def
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-28 20:30:52 +0700

    Update user.js

--- backend/src/user/entity/user.js
+++ backend/src/user/entity/user.js
@@ -2,79 +2,77 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import { sequelize } from '../../db/sequelize.js';
 
-const User = (sequelize, DataTypes) => {
-    return sequelize.define("User", {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        username: {
-            
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        profile_image: {
-            type: DataTypes.STRING,
-        },
-        avatar: {
-            type: DataTypes.STRING,
-        },
-        country: {
-            type: DataTypes.STRING,
-        },
-        city: {
-            type: DataTypes.STRING,
-        },
-        short_description: {
-            type: DataTypes.STRING,
-        },
-        firstname: {
-            type: DataTypes.STRING,
-        },
-        lastname: {
-            type: DataTypes.STRING,
-        },
-        about_me: {
-            type: DataTypes.TEXT,
-        },
-        contributions_counter: {
-            type: DataTypes.INTEGER,
-        },
-        campaigns_counter: {
-            type: DataTypes.INTEGER,
-        },
-        comments_counter: {
-            type: DataTypes.INTEGER,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: Sequelize.NOW,
-            field: 'created_at'
-        },
-    })
-}
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    username: {
+        
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    profile_image: {
+        type: DataTypes.STRING,
+    },
+    avatar: {
+        type: DataTypes.STRING,
+    },
+    country: {
+        type: DataTypes.STRING,
+    },
+    city: {
+        type: DataTypes.STRING,
+    },
+    short_description: {
+        type: DataTypes.STRING,
+    },
+    firstname: {
+        type: DataTypes.STRING,
+    },
+    lastname: {
+        type: DataTypes.STRING,
+    },
+    about_me: {
+        type: DataTypes.TEXT,
+    },
+    contributions_counter: {
+        type: DataTypes.INTEGER,
+    },
+    campaigns_counter: {
+        type: DataTypes.INTEGER,
+    },
+    comments_counter: {
+        type: DataTypes.INTEGER,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at'
+    },
+})
 
 export { User };
