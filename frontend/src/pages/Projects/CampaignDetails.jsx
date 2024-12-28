3f37a16e3f7cbe74c6b43d33b7434941300147bd
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-28 10:46:30 +0700

    Update CampaignDetails.jsx

--- frontend/src/pages/Projects/CampaignDetails.jsx
+++ frontend/src/pages/Projects/CampaignDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import graphql_api from "../../graphql";
+// import graphql_api from "../../graphql";
 import { useEffect, useState } from "react";
 import { 
     CampaignSection,
