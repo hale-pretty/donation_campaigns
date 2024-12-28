a5412c65ed1581f5a4d8f56364656cc83f4ac1a3
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-28 14:41:43 +0700

    Update user.js

--- backend/src/user/entity/user.js
+++ backend/src/user/entity/user.js
@@ -1,23 +1,67 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../../db/sequelize';
-
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  tableName: 'users',
-});
-
-export { User };
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("User", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        username: {
+            primaryKey: true,
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        profile_image: {
+            type: DataTypes.STRING,
+        },
+        avatar: {
+            type: DataTypes.STRING,
+        },
+        country: {
+            type: DataTypes.STRING,
+        },
+        city: {
+            type: DataTypes.STRING,
+        },
+        short_description: {
+            type: DataTypes.STRING,
+        },
+        firstname: {
+            type: DataTypes.STRING,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+        },
+        about_me: {
+            type: DataTypes.TEXT,
+        },
+        contributions_counter: {
+            type: DataTypes.INTEGER,
+        },
+        campaigns_counter: {
+            type: DataTypes.INTEGER,
+        },
+        comments_counter: {
+            type: DataTypes.INTEGER,
+        },
+    })
+}
