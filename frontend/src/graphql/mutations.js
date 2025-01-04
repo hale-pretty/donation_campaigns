e97921d3370788fa28f07ef6c8daa8a6df09a899
emerald-lan <emeraldlan1105@gmail.com>
2025-01-04 11:54:05 +0700

    integrate login user

--- frontend/src/graphql/mutations.js
+++ frontend/src/graphql/mutations.js
@@ -13,3 +13,11 @@ export const REGISTER_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+    }
+  }
+`;
