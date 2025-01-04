58f6162617dfe00d5513f32810052e8160daee2d
hale-pretty <lnhatha89@gmail.com>
2025-01-04 17:27:47 +0700

    Add websocket client for front end

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,4 +1,5 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
 const client = new ApolloClient({
   link: new HttpLink({
@@ -7,4 +8,42 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-export default client;
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const wsClient = new ApolloClient({
+  wsLink,
+  cache: new InMemoryCache(),
+});
+
+const DONATION_ADDED = gql`
+  subscription donationAdded($campaignId: Int!) {
+    donationAdded(campaignId: $campaignId) {
+      newDonation {
+        id
+        amount
+        createdAt
+      }
+      totalRaised
+    }
+  }
+`;
+
+// // Subscribe to the donation updates
+// wsClient.subscribe({
+//   query: DONATION_ADDED,
+//   variables: { campaignId: 1 },
+// }).subscribe({
+//   next(data) {
+//     console.log('New donation update:', data);
+//   },
+//   error(err) {
+//     console.error('Subscription error:', err);
+//   },
+// });
+
+export { client, wsClient };
