902c23c7777f566ea1b73d9dcc275d1b36c6ee19
emerald-lan <emeraldlan1105@gmail.com>
2025-01-05 23:23:56 +0700

    update cron job

--- backend/src/util/utils.test.js
+++
@@ -1,9 +0,0 @@
-import { add } from './utils';
-
-describe('add function', () => {
-  it('should add two numbers correctly', () => {
-    expect(add(1, 2)).toBe(3);
-    expect(add(-1, -1)).toBe(-2);
-    expect(add(0, 0)).toBe(0);
-  });
-});
